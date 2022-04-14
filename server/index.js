0542736a84456f994a5476782390a6d16ff77589
Dmitriy Demiankov <demankovdima480@gmail.com>
2022-04-14 11:56:54 +0300

    Создание бд

--- server/index.js
+++ server/index.js
@@ -1,8 +1,20 @@
 require('dotenv').config()
 const express = require('express')
+const sequelize = require('./db')
+
 
 const PORT = process.env.PORT
 
 const app = express()
 
-app.listen(PORT, ()=>console.log('Server started'))
+const start = async() => {
+    try{
+        await sequelize.authenticate()
+        await sequelize.sync()
+        app.listen(PORT, ()=>console.log('Server started'))
+    } catch{
+        console.log(e)
+    }
+}
+
+start()
---
+++ server/models/models.js
@@ -0,0 +1,2 @@
+const sequelize = require('../db')
+const {DataTypes} = require('sequelize')

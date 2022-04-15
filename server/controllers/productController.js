eec8d81c6adbf368c8db49ac532bdab26c00a65d
Dmitriy Demiankov <demankovdima480@gmail.com>
2022-04-16 00:33:28 +0300

    Получение устройств и фильтрация мебели

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -1,18 +1,29 @@
 const uuid = require('uuid')
 const path = require('path')
-const {Product} = require('../models/models')
+const {Product, ProductInfo} = require('../models/models')
 const ApiError = require('../error/ApiError')
 
 class ProductController{
     async create(req,res,next){
         try{
-            const {name, price, brandId,typeId,info} = req.body
+            let {name, price, brandId,typeId,info} = req.body
             const {img} = req.files
             let fileName = uuid.v4() + ".jpg" 
             img.mv(path.resolve(__dirname,'..','static',fileName))
-
-
             const product = await Product.create({name, price, brandId, typeId, img: fileName})
+
+            if (info){
+                info = JSON.parse(info)
+                info.forEach(i => 
+                  ProductInfo.create({
+                        title: i.title,
+                        description: i.description,
+                        productId: product.id                        
+                  })  
+                )
+            }
+
+            
         
             return res.json(product)
         }catch (e){
@@ -21,10 +32,35 @@ class ProductController{
     }
 
     async getAll(req,res){
-
+        let {brandId,typeId, limit, page} = req.query
+        page = page || 1
+        limit = limit || 9
+        let offset = page * limit - limit
+        let product;
+        if (!brandId && !typeId){
+            product = await Product.findAndCountAll({limit, offset})
+        }
+        if (brandId && !typeId){
+            product = await Product.findAndCountAll({where:{brandId}, limit, offset})
+        }
+        if (!brandId && typeId){
+            product = await Product.findAndCountAll({where:{typeId}, limit, offset})
+        }
+        if (brandId && typeId){
+            product = await Product.findAndCountAll({where:{typeId, brandId}, limit, offset})
+        }
+        return res.json(product)
     }
     async getOne(req,res){
+        const {id} = req.params
+        const product = await Product.findOne(
+            {
+                where: {id},
+                include: [{model: ProductInfo, as: 'info'}]
+            }
 
+        )
+        return res.json(product)
     }
     
 }
--- server/routes/productRouter.js
+++ server/routes/productRouter.js
@@ -4,6 +4,6 @@ const productController = require('../controllers/productController')
 
 router.post('/', productController.create)
 router.get('/', productController.getAll)
-router.get('/id',productController.getOne)
+router.get('/:id',productController.getOne)
 
 module.exports = router
